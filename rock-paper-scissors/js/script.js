8fb372390aecb130015c93766188d2aea616d9ac
Elijah Moye <Elijahmoye96@gmail.com>
2024-03-12 01:20:54 -0400

    IMAAAA A GODD
    if you havent notice i rediscover that im a coding
    genius everyday its crazy because i was waiting
    for this moment and its here and im eleated this
    has nothing to do qith the code,UI is finished
    just needs to be designed but every component
    is working in harmony.

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -4,23 +4,29 @@ let computerScore = 0;
 const options = document.getElementById("#options");
 const playerDisplay = document.getElementById("#playerDisplay");
 
+// Player one score ui i know this is considered bad code based off top im aware just let me be me for now
 const container = document.querySelector("#container");
 const div = document.createElement("div");
 div.classList.add("Paragraph");
 div.textContent = `Player ${playerScore}`;
 container.appendChild(div);
 
+// Computer Score Ui
 const computer = document.createElement("div");
 computer.classList.add("second");
 computer.textContent = `Computer ${computerScore}`;
 container.appendChild(computer);
 
+// Computer and playerSelection  results Ui
 const li = document.querySelector("#player");
+const opp = document.querySelector("#computer");
 
+// Winner results UI
+const winner = document.querySelector("#winner");
 
+// Outcome results UI
 
-
-
+const outcome = document.querySelector("#outcome");
 
 
 
@@ -32,94 +38,126 @@ function getComputerChoice() {
 
 function playGame(playerSelection) {
 
-     computerSeclection = getComputerChoice();
+   
+    computerSeclection = getComputerChoice();
+     
     let result = "";
 
     if (playerSelection === "rock" && computerSeclection === "scissors") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Win";
         console.log(result);
-        ++playerScore;
+        outcome.textContent = `${result}`;
+        playerScore++;
         console.log(playerScore);
         console.log(computerScore);
-       
+        keepScore();
     }
     else if (playerSelection === "paper" && computerSeclection === "rock") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Win";
         console.log(result);
-        ++playerScore;
+        outcome.textContent = `${result}`;
+        playerScore++;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     } 
     else if (playerSelection === "scissors" && computerSeclection === "paper") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Win";
         console.log(result);
-        ++playerScore;
+        outcome.textContent = `${result}`;
+        playerScore++;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     }
     else if (playerSelection === "rock" && computerSeclection === "paper") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Lose";
         console.log(result);
-        ++computerScore;
+        outcome.textContent = `${result}`;
+        computerScore++;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     }
     else if (playerSelection === "paper" && computerSeclection === "scissors") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Lose";
         console.log(result);
-        ++computerScore;
+        outcome.textContent = `${result}`;
+        computerScore++;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     }
     else if (playerSelection === "scissors" && computerSeclection === "rock") {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "You Lose";
         console.log(result);
-        ++computerScore;
+        outcome.textContent = `${result}`;
+        computerScore++;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     } 
     else if (playerSelection === computerSeclection) {
 
         console.log(playerSelection);
+        li.textContent = `PlayerOne chooses ${playerSelection}`;
         console.log(computerSeclection);
+        opp.textContent = `Computer chooses ${computerSeclection}`
         result = "Its a Tie";
         console.log(result);
+        outcome.textContent = `${result}`;
         console.log(playerScore);
         console.log(computerScore);
+        keepScore();
     }
     else {
         console.log("Please choose rock paper or scissors");
         console.log(playerScore);
         console.log(computerScore);
+        
     }
-    console.log("Press Up to play again");
+    console.log("RPS");
 
-    if (playerScore === 5 ) {
+    if (playerScore === 3 ) {
         console.log("PlayerOne Wins");
         console.log("Game Over");
+        winner.textContent = `PlayerOne Wins`;
         console.log(computerScore = 0);
         console.log(playerScore = 0);
-    } else if (computerScore === 5) {
+    } else if (computerScore === 3) {
         console.log("Computer Wins");
         console.log("Game Over");
+        winner.textContent = `Computer Wins`;
         console.log(computerScore = 0);
         console.log(playerScore = 0);
     }
@@ -128,39 +166,6 @@ function playGame(playerSelection) {
 
 function keepScore() {
     div.textContent = `Player ${playerScore}`;
-    computer.textContent = `Computer ${computerScore}`
+    computer.textContent = `Computer ${computerScore}`;
 
-    
 }
-
-    function game(playerSelection) {
-        const winner = document.querySelector("#winner");
-    
-
-
-        for (i = 0; i < 5; i++) {
-            let playerScore = 0;
-            let computerScore = 0;
-            console.log(playGame(playerSelection));
-            keepScore();
-            if (result === "You Win") {
-                ++playerScore
-            } else if (result === "You Lose") {
-                ++computerScore
-            }
-        
-        }
-
-        if (playerScore > computerScore) {
-            winner.textContent= `PlayerOne Wins`;
-        }
-        else if (playerScore < computerScore) {
-            winner.textContent = `Computer Wins`;
-        }
-        console.log("Score resets");
-        console.log(playerScore = 0);
-        console.log(computerScore = 0);
-        return ("Press up to play again");
-    }
-    
-  
